cdf6136548a677428264942e698b11c7d9b66adb
OurShiningDays <3406866634@qq.com>
2022-08-18 15:32:14 +0800

    更新关注列表功能1.添加性别 2.添加地址 3.图标尺寸改为圆形 4.布局调整

--- app/src/main/java/com/qxy/example/ui/user/FollowActivity.kt
+++ app/src/main/java/com/qxy/example/ui/user/FollowActivity.kt
@@ -2,11 +2,31 @@ package com.qxy.example.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.qxy.example.R
+import com.qxy.example.databinding.ActivityFollowBinding
+import com.qxy.example.databinding.ActivityMainBinding
 
 class FollowActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFollowBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_follow)
+        binding = ActivityFollowBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //底部Tab切换
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_follow)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_follow, R.id.navigation_follower))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/qxy/example/ui/user/FollowAdapter.kt
+++ app/src/main/java/com/qxy/example/ui/user/FollowAdapter.kt
@@ -1,5 +1,6 @@
 package com.qxy.example.ui.user
 
+import android.annotation.SuppressLint
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.qxy.example.CustomApplication
 import com.qxy.example.R
 import com.qxy.example.logic.model.FollowList
@@ -20,6 +23,8 @@ class FollowAdapter (private val fragment: Fragment, private val followList: Lis
 
         val avatar: ImageView = view.findViewById(R.id.avatarImg)
         val nickname: TextView = view.findViewById(R.id.nickNameText)
+        val location: TextView = view.findViewById(R.id.location)
+        val gender: TextView = view.findViewById(R.id.gender)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -30,10 +35,32 @@ class FollowAdapter (private val fragment: Fragment, private val followList: Lis
         return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val follow = followList[position]
         holder.nickname.text = follow.nickname
-        Glide.with(CustomApplication.context).load(follow.avatar).into(holder.avatar)
+        when(follow.gender){
+            1 -> holder.gender.text = "\u2642\uFE0F" //男
+            2 -> holder.gender.text = "\u2640\uFE0F" //女
+        }
+        var country = follow.country
+        var province = follow.province?.replace("省".toRegex(), "")//统一删除"省"
+        var city = follow.city?.replace("市".toRegex(), "")//统一删除"市"
+        if(country == "") country = null
+        if(province == "") province = null
+        if(city == "") city = null
+        if(country == null && province == null && city == null){ //API无地区返回数据则不展示
+            holder.location.text = ""
+        } else{
+            holder.location.text = "\uD83D\uDCCD $country ${province}省 ${city}市"
+                .replace("null省 ".toRegex(),"") //处理API未返回"省"情况
+                .replace("null市".toRegex(), "") //处理API未返回"市"情况
+                .replace("null".toRegex(),"") //处理遗漏null
+        }
+
+        Glide.with(CustomApplication.context).load(follow.avatar)
+            .apply(RequestOptions.bitmapTransform(CircleCrop())) //圆形效果
+            .into(holder.avatar)
     }
     override fun getItemCount() = followList.size
 }
